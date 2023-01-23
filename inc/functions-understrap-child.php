40e5a2babddc603ac8eb641ce43f981398f13796
Roman Sörensen <soerensen@tack-design.de>
2023-01-23 10:45:45 +0100

    moved enqueue and child-functions to separate File

---
+++ inc/functions-understrap-child.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Understrap Child-Theme functions and definitions
+ *
+ * @package tack-understrap-child
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Load the child theme's text domain
+ */
+function add_child_theme_textdomain() {
+	load_child_theme_textdomain( 'understrap-child', get_stylesheet_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
+
+
+
+/**
+ * Overrides the theme_mod to default to Bootstrap 5
+ *
+ * This function uses the `theme_mod_{$name}` hook and
+ * can be duplicated to override other theme settings.
+ *
+ * @return string
+ */
+function understrap_default_bootstrap_version() {
+	return 'bootstrap5';
+}
+add_filter( 'theme_mod_understrap_bootstrap_version', 'understrap_default_bootstrap_version', 20 );
+
+
+
+/**
+ * Loads javascript for showing customizer warning dialog.
+ */
+function understrap_child_customize_controls_js() {
+	wp_enqueue_script(
+		'understrap_child_customizer',
+		get_stylesheet_directory_uri() . '/js/customizer-controls.js',
+		array( 'customize-preview' ),
+		'20130508',
+		true
+	);
+}
+add_action( 'customize_controls_enqueue_scripts', 'understrap_child_customize_controls_js' );
+
---
+++ inc/tack-enqueue.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * tack enqueue scripts
+ *
+ * @package tack-understrap-child
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Removes the parent themes stylesheet and scripts from inc/enqueue.php
+ */
+function tack_remove_scripts() {
+	wp_dequeue_style( 'understrap-styles' );
+	wp_deregister_style( 'understrap-styles' );
+
+	wp_dequeue_script( 'understrap-scripts' );
+	wp_deregister_script( 'understrap-scripts' );
+}
+add_action( 'wp_enqueue_scripts', 'tack_remove_scripts', 20 );
+
+
+/**
+ * Enqueue our stylesheet and javascript file
+ */
+function theme_enqueue_styles() {
+
+	// Get the theme data.
+	$the_theme = wp_get_theme();
+
+	$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+	// Grab asset urls.
+	$theme_styles  = "/css/tack-theme{$suffix}.css";
+	$theme_scripts = "/js/tack-theme{$suffix}.js";
+
+	wp_enqueue_style( 'theme-styles', get_stylesheet_directory_uri() . $theme_styles, array(), $the_theme->get( 'Version' ) );
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'theme-scripts', get_stylesheet_directory_uri() . $theme_scripts, array(), $the_theme->get( 'Version' ), true );
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
